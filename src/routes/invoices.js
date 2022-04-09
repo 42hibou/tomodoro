71f082b0cfce0d1e99691ed0954c096ded9d5de2
42hibou <42hibou@gmail.com>
2022-04-09 04:05:25 +0200

    landing page layout

--- src/routes/invoices.js
+++ src/routes/invoices.js
@@ -3,10 +3,13 @@ import React from 'react';
 import {
   ChakraProvider,
   Flex,
+  Box,
   Spacer,
   Heading,
-  IconButton,
-  Text } from '@chakra-ui/react'
+  Stack,
+  Button,
+  Text,
+  Link } from '@chakra-ui/react'
 import theme from '../config/theme';
 import { SearchIcon } from '@chakra-ui/icons';
 
@@ -15,37 +18,41 @@ const Invoices = () => {
   return (
     <ChakraProvider theme={theme}>
       <Flex height='100vh' direction='column' align='center' justify='center' flexGrow='4'>
-        <div>
-        <Heading
-          as='h1' 
-          size='4xl'
-          // bgGradient='linear(to-r, teal.500, green.500)'
-          // bgClip='text'
-          // fontWeight='extrabold'
-          >tomodoro・ともドロ
-          </Heading>
-          <Text as='samp' fontSize='lg'>
-            Your Productivity Companion
-          </Text>
-          <Flex direction='row'>
-            <IconButton
-              colorScheme='blue'
-              aria-label='Search database'
-              icon={<SearchIcon />}
-            />
-            <Text>I know the way</Text> 
-            <IconButton
-              colorScheme='teal'
-              aria-label='Search database'
-              icon={<SearchIcon />}
-            />
-            <Text>How it works</Text>
-          </Flex>
-          <Text>
-            Tomodoro is free, open-source software that <Text as='u'>does not</Text> have access to any of your data.
-          </Text>
-          <Text as='u'>Support us.</Text>
-        </div>
+          <Box minWidth='660px' minHeight='330px'>
+
+          <Box height={'20vh'}>
+          <Heading
+            as='h1' 
+            size='4xl'
+            >
+              tomodoro・ともドロ
+            </Heading>
+            <Text as='samp' fontSize='lg'>
+              Your Productivity Companion
+            </Text>
+          </Box>
+          <Box height={'10vh'}>
+            {/* <Flex direction='row' justify='start'> */}
+            <Stack direction='row' spacing={4} align='center'>
+              <Button colorScheme='teal' size='lg'>
+                Get started
+              </Button>
+              <Button colorScheme='teal' size='lg' variant='outline'>
+                How it works?
+              </Button>
+            </Stack>
+
+            {/* </Flex>/ */}
+          </Box>
+          <Box height={'5vh'}>
+            <Text fontSize='sm' as='samp'>
+              Tomodoro is free, open-source software that does not have access to any of your data.
+            </Text>
+          </Box>
+          <Box  height={'5vh'}>
+            <Text fontSize='sm' as='samp' as='u'>Support me</Text>
+          </Box>
+        </Box>
       </Flex>
     </ChakraProvider>
   )
